65abf1ffe89228153a041050ca2cf540f1217f82
Darren James Dalino <darrenjames.dalino@gmail.com>
2020-04-15 23:31:06 +0700

    add book now button at navbar and add copy right at footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,45 +1,60 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Footer() {
   return (
     <div className="footer-container">
-      <div className="container-box text-light mt-5">
-        <div className="column footer-box">
-          <h4 className="text-uppercase mainText">sl roofing & renovations</h4>
-          <h4 className="text-uppercase secondaryText">
-            business 424-sl-roofs 424-757-6637
-          </h4>
-          <h4 className="text-uppercase secondaryText">cell 712-898-0221</h4>
-          <h4 className="text-uppercase secondaryText">
-            email slroofs@yahoo.com
-          </h4>
-        </div>
-        <div className="column footer-box">
-          <h4 className="text-uppercase mainText">sign up to our newsletter</h4>
-          <h4 className="text-capitalize secondaryText">
-            you'll receive inspirational ideas <br /> and advice for your home
-            renovation
-          </h4>
-          <div className="subscribe-container">
-            <input
-              type="text"
-              className="text-uppercase"
-              placeholder="enter you email..."
-              name="search"
-            />
-            <button type="submit" className="text-uppercase bg">
-              subscribe
-            </button>
+      <div className="footer-box-container">
+        <div className="container-box text-light">
+          <div className="column footer-box">
+            <h4 className="text-uppercase mainText">
+              sl roofing & renovations
+            </h4>
+            <h4 className="text-uppercase secondaryText">
+              business 424-sl-roofs 424-757-6637
+            </h4>
+            <h4 className="text-uppercase secondaryText">cell 712-898-0221</h4>
+            <h4 className="text-uppercase secondaryText">
+              email slroofs@yahoo.com
+            </h4>
+          </div>
+          <div className="column footer-box">
+            <h4 className="text-uppercase mainText">
+              sign up to our newsletter
+            </h4>
+            <h4 className="text-capitalize secondaryText">
+              you'll receive inspirational ideas <br /> and advice for your home
+              renovation
+            </h4>
+            <div className="subscribe-container">
+              <input
+                type="text"
+                className="text-uppercase"
+                placeholder="enter you email..."
+                name="search"
+              />
+              <button type="submit" className="text-uppercase bg">
+                subscribe
+              </button>
+            </div>
+          </div>
+          <div className="column footer-box">
+            <h4 className="text-uppercase mainText">stay connected with us</h4>
+            <h6 className="text-capitalize secondaryText">follow us on:</h6>
+            <div>
+              <i className="fab fa-linkedin-in mx-2" />
+              <i className="fab fa-facebook-f mx-2" />
+              <i className="fab fa-instagram mx-2"></i>
+            </div>
           </div>
         </div>
-        <div className="column footer-box">
-          <h4 className="text-uppercase mainText">stay connected with us</h4>
-          <h6 className="text-capitalize secondaryText">follow us on:</h6>
-          <div>
-            <i className="fab fa-linkedin-in mx-2" />
-            <i className="fab fa-facebook-f mx-2" />
-            <i className="fab fa-instagram mx-2"></i>
-          </div>
+        <div className="col footer-copy-right text-light">
+          <p className="text-capitalize text-center">
+            copyright@2020{" "}
+            <Link className="secondaryText" to="/">
+              SLROOFING
+            </Link>{" "}
+            All Rights Reserved
+          </p>
         </div>
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from "../img/LOGO.jpg";
 import { Link } from "react-router-dom";
 function Navbar() {
   return (
-    <nav className="navbar fixed-top navbar-expand-md navbar-expand-sm navbar-light bg-light px-sm-5 mb-5">
+    <nav className="navbar fixed-top navbar-expand-md navbar-expand-sm navbar-light bg-light px-sm-5 ">
       <div className="container-box">
         <Link to="/" className="navbar-brand icon-logo">
           <img src={logo} alt="" />
@@ -11,24 +11,22 @@ function Navbar() {
         </Link>
 
         <div className="navbar-nav ml-auto">
-          <Link to="/" className="nav-item nav-link mx-4">
+          <Link to="/" className="nav-item nav-link mx-4 my-auto">
             Home
           </Link>
-          <Link to="/" className="nav-item nav-link mx-4">
+          <Link to="/" className="nav-item nav-link mx-4 my-auto">
             About
           </Link>
-          <Link to="/" className="nav-item nav-link mx-4">
+          <Link to="/" className="nav-item nav-link mx-4 my-auto">
             Contact Us
           </Link>
-          <Link to="/" className="nav-item nav-link mx-4">
+          <Link to="/" className="nav-item nav-link mx-4 my-auto">
             Services
           </Link>
-          <Link to="/" className="nav-item nav-link mx-4">
+          <Link to="/" className="nav-item nav-link mx-4 my-auto">
             Blog
           </Link>
-          <Link to="/" className="nav-item nav-link mx-4">
-            Products
-          </Link>
+          <button className="btn-book text-uppercase">book now</button>
         </div>
       </div>
     </nav>
