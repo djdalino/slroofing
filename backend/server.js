f794c56a3af1c588f4e4e7d2a6c062fc27521508
Darren James Dalino <darrenjames.dalino@gmail.com>
2020-05-02 22:43:45 +0700

    update

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ mongoose.connect(
 );
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
+  app.use(express.static("../build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"));
