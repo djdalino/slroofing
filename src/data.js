6e6361fc0901e4bd8d8837813751052be8c23634
Hanna Claros <hannaclaros@Hannas-MacBook-Air.local>
2020-05-07 01:05:11 +0800

    modified homepage 1:04 am

--- src/data.js
+++ src/data.js
@@ -1,59 +1,24 @@
 import Roofing from "./img/thumbnail-1.png";
-import Siding from "./img/thumbnail-2.png";
-import InteriorDesign from "./img/thumbnail-3.png";
-import Garages from "./img/thumbnail-4.png";
-import Decks from "./img/thumbnail-5.png";
-import OutdoorKitchens from "./img/thumbnail-6.png";
-import RetainingWalls from "./img/thumbnail-7.png";
-import Landscaping from "./img/thumbnail-8.png";
+import Gutters from "./img/thumbnail-2.png";
+import Siding from "./img/thumbnail-7.png";
 
 export const servicesOffer = [
   {
     id: 1,
     title: "Roofing",
     img: Roofing,
-    category: ["Roofing"],
+    category: ["Roofing"]
   },
   {
     id: 2,
     title: "Siding",
     img: Siding,
-    category: ["Siding"],
+    category: ["Siding"]
   },
   {
     id: 3,
-    title: "Interior Design",
-    img: InteriorDesign,
-    category: ["Interior Design"],
-  },
-  {
-    id: 4,
-    title: "Garages",
-    img: Garages,
-    category: ["Garages"],
-  },
-  {
-    id: 5,
-    title: "Decks",
-    img: Decks,
-    category: ["Decks"],
-  },
-  {
-    id: 6,
-    title: "Outdoor Kitchens",
-    img: OutdoorKitchens,
-    category: ["Outdoor Kitchens"],
-  },
-  {
-    id: 7,
-    title: "Retaining Walls",
-    img: RetainingWalls,
-    category: ["Retaining Walls"],
-  },
-  {
-    id: 8,
-    title: "Landscaping",
-    img: Landscaping,
-    category: ["Landscaping"],
-  },
+    title: "Gutters",
+    img: Gutters,
+    category: ["Gutters"]
+  }
 ];
