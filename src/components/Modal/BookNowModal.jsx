6df0bb79462bc5a6f050fedafc78f3410db570c0
Hanna Claros <hannaclaros@Hannas-MacBook-Air.local>
2020-05-10 02:00:18 +0800

    modified 2am

--- src/components/Modal/BookNowModal.jsx
+++ src/components/Modal/BookNowModal.jsx
@@ -19,13 +19,13 @@ class BookNowModal extends Component {
     address: "",
     city: "",
     stateValue: "",
-    postal_code: "",
+    postal_code: ""
   };
-  setNewValue = (newValue) => {
+  setNewValue = newValue => {
     this.setState({ stateValue: newValue });
   };
 
-  handleSubmitBookNow = async (e) => {
+  handleSubmitBookNow = async e => {
     const data = {
       startDate: this.state.startDate,
       pickTime: this.state.pickTime,
@@ -36,7 +36,7 @@ class BookNowModal extends Component {
       address: this.state.address,
       city: this.state.city,
       stateValue: this.state.stateValue,
-      postal_code: this.state.postal_code,
+      postal_code: this.state.postal_code
     };
     console.log(data);
     try {
@@ -50,7 +50,7 @@ class BookNowModal extends Component {
       alert(error);
     }
   };
-  getDate = (dateString) => {
+  getDate = dateString => {
     const month = [
       "January",
       "February",
@@ -63,14 +63,14 @@ class BookNowModal extends Component {
       "September",
       "October",
       "November",
-      "December",
+      "December"
     ];
     const date = new Date(dateString);
 
     return `${month[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
   };
 
-  setStartDate = (date) => {
+  setStartDate = date => {
     this.setState({ startDate: date });
   };
   nextStep = () => {
@@ -79,12 +79,12 @@ class BookNowModal extends Component {
   prevStep = () => {
     this.setState({ step: this.state.step - 1 });
   };
-  handleChange = (event) => {
+  handleChange = event => {
     this.setState({
-      [event.target.name]: event.target.value,
+      [event.target.name]: event.target.value
     });
   };
-  handleInputChange = (e) => {
+  handleInputChange = e => {
     this.setState({ pickTime: e.target.value });
     console.log(e.target.value);
   };
@@ -100,7 +100,7 @@ class BookNowModal extends Component {
       address,
       city,
       stateValue,
-      postal_code,
+      postal_code
     } = this.state;
     const values = {
       startDate,
@@ -112,7 +112,7 @@ class BookNowModal extends Component {
       address,
       city,
       stateValue,
-      postal_code,
+      postal_code
     };
     console.log(this.state);
     switch (step) {
@@ -121,7 +121,7 @@ class BookNowModal extends Component {
           <div className="book-now-modal">
             <div className="book-now-modal-container">
               <div className="book-now-datetime">
-                <h1 className="text-center">Pick Date</h1>
+                <h1 className="pick">Pick Date</h1>
                 <FormDatePick
                   values={values}
                   setStartDate={this.setStartDate}
@@ -140,7 +140,7 @@ class BookNowModal extends Component {
           <div className="book-now-modal">
             <div className="book-now-modal-container">
               <div className="book-now-datetime">
-                <h1 className="text-center">Pick Time</h1>
+                <h1 className="pick">Pick Time</h1>
                 <FormTimePick
                   values={values}
                   nextStep={this.nextStep}
@@ -158,7 +158,7 @@ class BookNowModal extends Component {
           <div className="book-now-modal">
             <div className="book-now-modal-container">
               <div className="book-now-form-info">
-                <h1 className="text-center">Your Information</h1>
+                <h1 className="pick">Your Information</h1>
                 <FormInfo
                   values={values}
                   setStartDate={this.setStartDate}
@@ -178,7 +178,7 @@ class BookNowModal extends Component {
           <div className="book-now-modal">
             <div className="book-now-modal-container">
               <div className="book-now-form-info">
-                <h1 className="text-center">Your Information</h1>
+                <h1 className="pick">Your Information</h1>
                 <FormDetails
                   values={values}
                   setStartDate={this.setStartDate}
@@ -199,7 +199,7 @@ class BookNowModal extends Component {
           <div className="book-now-modal">
             <div className="book-now-modal-container">
               <div className="book-now-form-info">
-                <h1 className="text-center">Success</h1>
+                <h1 className="suc">Success</h1>
                 <FormSuccess
                   values={values}
                   setStartDate={this.setStartDate}
