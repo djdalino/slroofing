a56e13177abd0a479db2164e58fdc938b8c9e92b
Hanna Claros <hannaclaros@Hannas-MacBook-Air.local>
2020-05-19 00:38:36 +0800

    allgood

--- src/components/Services/servicesOffer.jsx
+++ src/components/Services/servicesOffer.jsx
@@ -8,24 +8,24 @@ class servicesOffer extends Component {
     return (
       <div>
         <div className="who-we-are">
-          <h5 className="text-uppercase">What are you looking for?</h5>
+          <h5 className="text-uppercase">What services are you looking for?</h5>
           <h2 className="text-center text-capitalize mainText">
             Our services we offer
           </h2>
           <br />
           <div className="row">
             <ProductConsumer>
-              {(value) => {
+              {value => {
                 const { servicesOffer } = value;
                 const arrayCategory = ["all"];
-                servicesOffer.forEach((item) => {
-                  item.category.forEach((category) => {
+                servicesOffer.forEach(item => {
+                  item.category.forEach(category => {
                     if (arrayCategory.indexOf(category) < 0) {
                       arrayCategory.push(category);
                     }
                   });
                 });
-                return arrayCategory.map((category) => {
+                return arrayCategory.map(category => {
                   return (
                     <ServicesOfferColumn
                       key={category}
