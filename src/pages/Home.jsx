b8e2ea33019638928d066b569936143d79bb3188
Darren James Dalino <darrenjames.dalino@gmail.com>
2020-06-22 15:50:35 +0700

    update css and banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,46 +13,45 @@ const Home = () => {
   return (
     <React.Fragment>
       <Navbar />
-      <div>
-        <Hero>
-          <div className="banner-container">
-            <div className="box-container">
-              <h1 className="text-uppercase">Done right!</h1>
-              <h1 className="text-uppercase">Watertight!</h1>
-              <p>
-                <strong>COMMERCIAL - RESIDENTIAL ROOFING</strong>
-                <br /> <br />
-                Innovative solutions and design for all your roofing,
-                restoration and construction needs!
-              </p>
-              <div className="button-container book-an-appointment">
-                <ProductConsumer>
-                  {(value) => {
-                    const { isBookNow, handleBookNowModal } = value;
-                    return (
-                      <React.Fragment>
-                        <Link
-                          to="#"
-                          className="book-an-appointment btn btn-danger text-uppercase"
-                          onClick={handleBookNowModal}
-                        >
-                          book an appointment
-                        </Link>
 
-                        {isBookNow ? <BookNowModal /> : null}
-                      </React.Fragment>
-                    );
-                  }}
-                </ProductConsumer>
-              </div>
+      <Hero>
+        <div className="banner-container">
+          <div className="box-container">
+            <h1 className="text-uppercase">Done right!</h1>
+            <h1 className="text-uppercase">Watertight!</h1>
+            <p>
+              <strong>COMMERCIAL - RESIDENTIAL ROOFING</strong>
+              <br /> <br />
+              Innovative solutions and design for all your roofing, restoration
+              and construction needs!
+            </p>
+            <div className="button-container book-an-appointment">
+              <ProductConsumer>
+                {(value) => {
+                  const { isBookNow, handleBookNowModal } = value;
+                  return (
+                    <React.Fragment>
+                      <button
+                        className="book-an-appointment btn btn-danger text-uppercase"
+                        onClick={handleBookNowModal}
+                      >
+                        book an appointment
+                      </button>
+
+                      {isBookNow ? <BookNowModal /> : null}
+                    </React.Fragment>
+                  );
+                }}
+              </ProductConsumer>
             </div>
           </div>
-          <Banner></Banner>
-        </Hero>
-        <OurServices />
-        <Subscribe />
-        <Testimonial />
-      </div>
+        </div>
+        <Banner></Banner>
+      </Hero>
+      <OurServices />
+      <Subscribe />
+      <Testimonial />
+
       <Footer />
     </React.Fragment>
   );
