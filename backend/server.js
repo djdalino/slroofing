3b32c7238415591d04ede86bf2d44d354ea38d1a
Darren James Dalino <darrenjames.dalino@gmail.com>
2020-07-09 15:46:54 +0700

    update server

--- backend/server.js
+++ backend/server.js
@@ -14,70 +14,6 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//Contact Us
-
-app.post("/api/contactUs", (req, res) => {
-  const data = req.body;
-  const auth = {
-    auth: {
-      api_key: process.env.API_KEY,
-      domain: process.env.DOMAIN,
-    },
-  };
-
-  const transporter = nodemailer.createTransport(mg(auth));
-
-  const mailOptions = {
-    from: `${data.name} <${data.email}>`,
-    to: "darrenjames.dalino@gmail.com",
-    subject: `<strong>SL Roofing Query</strong>`,
-    html: `<p>Name: ${data.name}</p>
-        <p>Email From: ${data.email}</p>
-        <p>Phone: ${data.phone}</p>
-        <p>${data.inquiry}</p>`,
-  };
-
-  transporter.sendMail(mailOptions, (err, response) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("success");
-    }
-    transporter.close();
-  });
-});
-
-// Booking
-app.post("/api/booking", (req, res) => {
-  const data = req.body;
-  const auth = {
-    auth: {
-      api_key: process.env.API_KEY,
-      domain: process.env.DOMAIN,
-    },
-  };
-
-  const transporter = nodemailer.createTransport(mg(auth));
-
-  const mailOptions = {
-    from: `${data.name} <${data.email}>`,
-    to: "darrenjames.dalino@gmail.com",
-    subject: "SL Roofing Booking",
-    html: `<p>Name: ${data.name}</p>
-        <p>Email From: ${data.email}</p>
-        <p>Phone: ${data.phone}</p>
-        <p>${data.inquiry}</p>`,
-  };
-
-  transporter.sendMail(mailOptions, (err, response) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("success");
-    }
-    transporter.close();
-  });
-});
 // set up routes
 const post = require("./routes/postRoutes");
 const postCategory = require("./routes/postCategoryRoute");
@@ -97,10 +33,9 @@ app.listen(PORT, () => {
   console.log(`server is runnning on port: ${PORT}`);
 });
 // set up db connection
-const MONGODB_URL =
-  "mongodb+srv://djdalino:TpW5Z6aYjCENviOt@cluster3-hngry.mongodb.net/test?retryWrites=true&w=majority";
+
 mongoose.connect(
-  MONGODB_URL || process.env.MONGODB_URL,
+  process.env.MONGODB_URL,
   { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
   (err) => {
     if (err) return console.error(err);
